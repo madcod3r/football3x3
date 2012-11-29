0ec439504c5d81db5638935ee9f87481574ecb50
Oleg Voloshin <vololeg93@gmail.com>
2012-11-29 16:16:07 +0200

    initial commit

---
+++ src/Football/NewsBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Football\NewsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('FootballNewsBundle:Default:index.html.twig');
+    }
+
+	public function helloAction($name)
+	{
+		return $this->render('FootballNewsBundle:Default:hello.html.twig', array('name' => $name));
+	}
+}
---
+++ src/Football/NewsBundle/FootballNewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Football\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FootballNewsBundle extends Bundle
+{
+}
