fd99544cbd0da6d1a8f5fc5baede06f20e50d71b
Oleg Voloshin <vololeg93@gmail.com>
2012-12-26 04:55:59 +0200

    show leagues from database in homepage

--- src/Football/NewsBundle/Controller/DefaultController.php
+++ src/Football/NewsBundle/Controller/DefaultController.php
@@ -6,20 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-	    $events = $this->getDoctrine()->getRepository('FootballNewsBundle:Event')->findBy(array(), array('order' => 'asc'));
+  public function indexAction()
+  {
+    $events = $this->getDoctrine()->getRepository('FootballNewsBundle:Event')->findBy(array(), array('order' => 'asc'));
+    $leagues = $this->getDoctrine()->getRepository('FootballNewsBundle:League')->findBy(array(), array('order' => 'asc'));
 
-        return $this->render('FootballNewsBundle:Default:index.html.twig', array('events' => $events));
-    }
+    return $this->render('FootballNewsBundle:Default:index.html.twig', array('events' => $events, 'leagues' => $leagues));
+  }
 
-	public function helloAction($name)
-	{
-		return $this->render('FootballNewsBundle:Default:hello.html.twig', array('name' => $name));
-	}
+  public function helloAction($name)
+  {
+    return $this->render('FootballNewsBundle:Default:hello.html.twig', array('name' => $name));
+  }
 
-	public function navigationAction()
-	{
-		return $this->render('FootballNewsBundle:Default:navigation.html.twig');
-	}
+  public function navigationAction()
+  {
+    return $this->render('FootballNewsBundle:Default:navigation.html.twig');
+  }
 }
