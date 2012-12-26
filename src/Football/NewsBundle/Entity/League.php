373ad9dc759d21c1e082638ecfd7c206d5369a7d
Oleg Voloshin <vololeg93@gmail.com>
2012-12-26 03:42:09 +0200

    added new entity - League

---
+++ src/Football/NewsBundle/Entity/League.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Football\NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * League
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class League
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="short_name", type="string", length=255)
+     */
+    private $short_name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="order", type="integer", length=3)
+     */
+    private $order;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return League
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set short_name
+     *
+     * @param string $shortName
+     * @return League
+     */
+    public function setShortName($shortName)
+    {
+        $this->short_name = $shortName;
+    
+        return $this;
+    }
+
+    /**
+     * Get short_name
+     *
+     * @return string 
+     */
+    public function getShortName()
+    {
+        return $this->short_name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return League
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set order
+     *
+     * @param integer $order
+     * @return League
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+    
+        return $this;
+    }
+
+    /**
+     * Get order
+     *
+     * @return integer 
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
